bad3c96f538c198a522269fe168d94f08b3f5d06
Juti Buican <jbuican19@gmail.com>
2021-08-08 10:45:00 +0100

    Creating module (refactor)

--- index.js
+++ index.js
@@ -5,21 +5,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// Create a same way we create a server ch. 12 Routing
+const replaceTemplate = require('./modules/replaceTemplate');
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
+// Create a same way we create a server ch. 12 Routing
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
